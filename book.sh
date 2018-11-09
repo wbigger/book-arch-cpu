b1aebdd5b56cd75cbb2580a5aecc66f5e17e6815
wbigger <wbigger@gmail.com>
2018-11-09 13:30:40 +0100

    Auto-publish book

--- book.sh
+++ book.sh
@@ -93,7 +93,11 @@ if [ $autobuild = true ]; then
   echo "Building book..."
   export MDBOOK_BUILD__BUILD_DIR="book-$CLASS"
   mdbook build || exit -1;
+  echo "Generating zip file..."
   zip -r -X book.zip book-$CLASS > /dev/null || exit -1;
+  echo "Copying book to docs..."
+  rm -rf ./docs
+  cp -r book-$CLASS docs
 else
   echo "Now you can build the book with the following command:";
   echo "export MDBOOK_BUILD__BUILD_DIR="book-$CLASS"; mdbook build"
